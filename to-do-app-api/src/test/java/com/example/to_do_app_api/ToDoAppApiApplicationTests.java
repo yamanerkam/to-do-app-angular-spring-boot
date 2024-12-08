8f95adf512d6816c1192abac38b66694c726d76f
yamanerkam <erkamyaman35@gmail.com>
2024-12-08 21:36:03 +0300

    backend-created

---
+++ to-do-app-api/src/test/java/com/example/to_do_app_api/ToDoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
