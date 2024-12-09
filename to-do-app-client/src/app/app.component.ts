266879688ddced69cbc8e17e5687bc1a2f17a713
yamanerkam <erkamyaman35@gmail.com>
2024-12-09 21:18:46 +0300

    delete-confirmation-dialogue-added

--- to-do-app-client/src/app/app.component.ts
+++ to-do-app-client/src/app/app.component.ts
@@ -3,16 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { Dialog } from 'primeng/dialog';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule, ButtonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, ButtonModule, InputTextModule, ConfirmDialog, Dialog],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true
+  standalone: true,
+  providers: [ConfirmationService]
 })
 export class AppComponent {
+
+  constructor(private confirmationService: ConfirmationService) { }
+
   title = 'toDoApp-client';
   taskName: string = '';
   tasks: string[] = ['a', 'u']
@@ -30,9 +38,18 @@ export class AppComponent {
   }
 
   deleteTheTask(task: any) {
-    // confirmation dialogue will be added here
-    const indexOfTask = this.tasks.findIndex((t) => t == task);
-    this.tasks.splice(indexOfTask, 1);
+    console.log()
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        const indexOfTask = this.tasks.findIndex((t) => t == task);
+        this.tasks.splice(indexOfTask, 1);
+      },
+      reject: () => { return }
+    });
+
   }
 
 
